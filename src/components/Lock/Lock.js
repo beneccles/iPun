6c76e91ba2a8831d4324613b37797a4035d61875
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-12 22:54:35 -0700

    commented in requirements for lockscreen component.

---
+++ src/components/Lock/Lock.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Lock.scss';
+
+function Lock() {
+    // This is the Lockscreen Component of the iPun. In web terms,
+    // this is going to be our landing page for the app.
+    // Requirements for this component:
+    // - Display Status Bar at the top of the screen (time, wifi, cell, battery)
+    // - Display lock, time and date in middle.
+    // - Display notifications.
+    // - Display buttons for flashlight and camera, even if they don't do anything.
+    // - Display chinbar.
+    
+  return (
+    <div className="Lock">
+      
+    </div>
+  );
+}
+
+export default Lock;
