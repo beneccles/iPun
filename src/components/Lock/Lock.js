0fe674edecd255ab98b83394e62d60f8aa14426b
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-13 21:40:27 -0700

    added switch statement for building date on the lockscreen.

--- src/components/Lock/Lock.js
+++ src/components/Lock/Lock.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Lock.scss';
 
-function Lock() {
+export default class Lock extends Component{
     // This is the Lockscreen Component of the iPun. In web terms,
     // this is going to be our landing page for the app.
     // Requirements for this component:
@@ -10,12 +10,71 @@ function Lock() {
     // - Display notifications.
     // - Display buttons for flashlight and camera, even if they don't do anything.
     // - Display chinbar.
-    
-  return (
-    <div className="Lock">
-      
+    // const [date, setDate] = useState(new Date());
+    constructor() {
+        super()
+        this.state = {
+            time: null
+        }
+    }
+
+    componentDidMount() {
+        let data = new Date();
+        let time = `${data.getHours()}:${data.getMinutes()}`
+        let date = ""
+
+        switch(data.getMonth()){
+            case 0:
+                date += "January"
+                break;
+            case 1:
+                date += "Feburary"
+                break;
+            case 2:
+                date += "March"
+                break;
+            case 3:
+                date += "April"
+                break;
+            case 4:
+                date += "May"
+                break;
+            case 5:
+                date += "June"
+                break;
+            case 6:
+                date += "July"
+                break;
+            case 7:
+                date += "August"
+                break;
+            case 8:
+                date += "September"
+                break;
+            case 9:
+                date += "October"
+                break;
+            case 10:
+                date += "November"
+                break;
+            case 11:
+                date += "December"
+                break;
+            default:
+                break;
+        }
+        console.log(data.getDay())
+        this.setState({
+            time: data
+        })
+    }
+
+render() {
+    return (
+        <div className="Lock">
     </div>
   );
 }
+}
 
-export default Lock;
+
