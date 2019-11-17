b21107e71be4ea51e42ed5410e059f24689257dd
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-16 17:14:57 -0700

    added additional styling for app placement on home screen.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,90 @@ export default function Home() {
     return (
         <div className="Home">
             <div id="appHome">
+                <div className="appRow">
+                    <div className="appLink">
 
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
+                <div className="appRow">
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
+                <div className="appRow">
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
+                <div className="appRow">
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
+                <div className="appRow">
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
+                <div className="appRow">
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                    <div className="appLink">
+
+                    </div>
+                </div>
             </div>
             <Dock />
         </div>
