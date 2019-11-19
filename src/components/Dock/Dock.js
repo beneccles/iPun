aebfb25982899d7da759170bde721249fdedd31b
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-18 19:59:28 -0700

    added phone icon to button link

--- src/components/Dock/Dock.js
+++ src/components/Dock/Dock.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import phoneImage from '../../assets/iconmonstr-phone-1.svg'
 import './Dock.scss';
 
 export default function Dock() {
     return (
         <div className="Dock">
-            <Link to="/Phone" className="appLink">
+            <Link id="Phone" to="/Phone" className="appLink">
+                <img src={phoneImage} alt="Phone Image" />
             </Link>
             <div className="appLink">
                 
