9252a4e2a05fc3c11d910512a26585d6d54ac9a4
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-19 22:39:58 -0700

    added svg icons to phone portion of the app.

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import star from '../../assets/iconmonstr-star-3.svg'
+import recent from '../../assets/iconmonstr-clock-thin.svg'
+import voicemail from '../../assets/voicemail-24px.svg'
 import "./Phone.scss"
 
 export default function Phone() {
@@ -9,12 +11,15 @@ export default function Phone() {
             </div>
             <div id="phone-dock">
                 <div className="phoneLink">
+                    <img src={star} alt="Favorites" />
+                </div>
+                <div className="phoneLink">
+                    <img src={recent} alt="Recent" />
                 </div>
                 <div className="phoneLink">
                 </div>
                 <div className="phoneLink">
-                </div>
-                <div className="phoneLink">
+                    <img src={voicemail} alt="Voicemail" />
                 </div>
             </div>
         </div>
