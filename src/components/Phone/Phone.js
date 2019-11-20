d9b6facb768b504e0c04a92e1e374e79783a62dd
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-19 22:27:10 -0700

    added mockup for phone app

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -5,7 +5,18 @@ import "./Phone.scss"
 export default function Phone() {
     return (
         <div className="Phone">
-
+            <div id="phone-home">
+            </div>
+            <div id="phone-dock">
+                <div className="phoneLink">
+                </div>
+                <div className="phoneLink">
+                </div>
+                <div className="phoneLink">
+                </div>
+                <div className="phoneLink">
+                </div>
+            </div>
         </div>
     )
 }
