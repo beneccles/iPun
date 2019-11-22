1fa6fe9f6d8e3eec4035ab8d61a00d6a81fff1e7
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-22 12:59:41 -0700

    fixed issue preventing phone keypad from rendering.

--- src/components/Phone/Keypad/Keypad.js
+++ src/components/Phone/Keypad/Keypad.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Keypad.scss";
+import circle from '../../../assets/iconmonstr-circle-1.svg'
 
 export default function Keypad() {
     return (
         <div className="Keypad">
-            
+            <div id="number-output">
+            </div>
+            <div id="first-row">
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+            </div>
+            <div id="second-row">
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+            </div>
+            <div id="third-row">
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+                <img src={circle} alt="keypadNum"/>
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,21 @@ import React from 'react';
 import Lock from './components/Lock/Lock'
 import Home from './components/Home/Home'
 import Phone from './components/Phone/Phone'
+import Keypad from './components/Phone/Keypad/Keypad'
+import Contacts from './components/Phone/Contacts/Contacts'
+import Favorites from './components/Phone/Favorites/Favorites'
+import Recents from './components/Phone/Recents/Recents'
+import Voicemail from './components/Phone/Voicemail/Voicemail'
 
 export default (
     <Switch>
         <Route exact path="/" component={Lock} />
         <Route path="/home" component={Home} />
         <Route path="/phone" component={Phone} />
+        <Route path="/keypad" component={Keypad} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/recents" component={Recents} />
+        <Route path="/voicemail" component={Voicemail} />
     </Switch>
 )
