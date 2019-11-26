bc30cdd499265c5ed5cb89ad0d139c079c920887
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-25 20:32:06 -0700

    added handlechange function for toCall number

--- src/components/Phone/Keypad/Keypad.js
+++ src/components/Phone/Keypad/Keypad.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import star from '../../../assets/iconmonstr-star-3.svg'
 import recent from '../../../assets/iconmonstr-clock-thin.svg'
@@ -11,49 +11,59 @@ import "./Keypad.scss";
 import circle from '../../../assets/iconmonstr-circle-1.svg'
 
 export default function Keypad() {
+
+    const [number, setNumber] = useState("");
+
+    function setOutgoingNumber(num) {
+        // Set the number into the call stack.
+        setNumber(number + num)
+        
+    }
+
     return (
         <div className="Keypad">
-            <div id="number-output">
+            <div id="number">
+                <h1 id="number-output"></h1>
             </div>
             <div id="keypad-buttons">
                 <div id="first-row">
-                    <div className="keypad-button first-column">
+                    <div className="keypad-button first-column" onClick={() => setOutgoingNumber("1")}>
                         <h1>1</h1>
                        <div></div>
                     </div>
-                    <div className="keypad-button second-column">
+                    <div className="keypad-button second-column" onClick={() => setOutgoingNumber("2")}>
                         <h1>2</h1>
                         <p>A B C</p>
                     </div>
-                    <div className="keypad-button">
+                    <div className="keypad-button" onClick={() => setOutgoingNumber("3")}>
                         <h1>3</h1>
                         <p>D E F</p>
                     </div>
                 </div>
                 <div id="second-row">
-                    <div className="keypad-button first-column">
+                    <div className="keypad-button first-column" onClick={() => setOutgoingNumber("4")}>
                         <h1>4</h1>
                         <p>G H I</p>
                     </div>
-                    <div className="keypad-button second-column">
+                    <div className="keypad-button second-column" onClick={() => setOutgoingNumber("5")}>
                         <h1>5</h1>
                         <p>J K L</p>
                     </div>
-                    <div className="keypad-button">
+                    <div className="keypad-button" onClick={() => setOutgoingNumber("6")}>
                         <h1>6</h1>
                         <p>M N O</p>
                     </div>
                 </div>
                 <div id="third-row">
-                    <div className="keypad-button first-column">
+                    <div className="keypad-button first-column" onClick={() => setOutgoingNumber("7")}>
                         <h1>7</h1>
                         <p>P Q R S</p>
                     </div>
-                    <div className="keypad-button second-column">
+                    <div className="keypad-button second-column" onClick={() => setOutgoingNumber("8")}>
                         <h1>8</h1>
                         <p>T U V</p>    
                     </div>
-                    <div className="keypad-button">
+                    <div className="keypad-button" onClick={() => setOutgoingNumber("9")}>
                          <h1>9</h1>
                          <p>W X Y Z</p>   
                     </div>
@@ -62,7 +72,7 @@ export default function Keypad() {
                     <div className="keypad-button first-column" id="star">
                         <img src={asterisk} alt="asterisk symbol" />
                     </div>
-                    <div className="keypad-button second-column">
+                    <div className="keypad-button second-column" onClick={() => setOutgoingNumber("0")}>
                         <h1>0</h1>
                         <p>+</p>          
                     </div>
