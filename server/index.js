3232f6ca86f4c8164209430c63618724ad1558cb
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-28 21:47:23 -0700

    completed boilerplate for server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
+const {SERVER_PORT, SESSION_SECRET} = process.env
+const app = express()
+
+app.use(express.json())
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET
+}))
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
