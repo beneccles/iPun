904e47d4fbf9202ee77e7c6cbecdcb87196e3b65
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-30 22:11:41 -0700

    started initial call control function in server

--- server/controllers/callController.js
+++ server/controllers/callController.js
@@ -1,3 +1,13 @@
+const {TWILIO_SID, AUTH_TOKEN} = process.env
 module.exports = {
-    
+    async makeCall (req, res) {
+        const client = require('twilio')(TWILIO_SID, AUTH_TOKEN)
+        const {to, from} = req.body
+        client.calls.create({
+            to: to,
+            from: from
+        })
+        .then(call => {console.log(`Call ${call.sid} Initiated`)})
+
+    }
 }
